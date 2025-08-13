847ad5c4db3a6f491e89703c87df02f4cd489998
SuperYazeed <yzd.qsm@gmail.com>
2025-08-13 10:48:04 -0700

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,14 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    plugins {
+        id("com.android.application") version "8.2.0"
+        id("org.jetbrains.kotlin.android") version "1.9.23"
+    }
+}
+
 rootProject.name = "IosStyleKeyboard"
 include(":app")
